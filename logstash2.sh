d40fb3b8c390df8eac7850a1c30e610d5a44486c
a.kim <alexander.kim.ca@gmail.com>
2018-05-03 14:41:59 -0400

    update

--- logstash2.sh
+++ logstash2.sh
@@ -2,7 +2,7 @@
 docker kill logstash-gc
 docker rm logstash-gc
 docker run -d --name logstash-gc -p 5044:5044 \
- -v config/logstash-gc.yml:/usr/share/logstash/config/logstash.yml \
- -v config/logstash-gc.conf:/usr/share/logstash/pipeline/logstash.conf \
- -v config/interim:/usr/share/logstash/vendor/bundle/jruby/2.3.0/gems/logstash-patterns-core-4.1.2/patterns/interim \
+ -v $(pwd)/config/logstash-gc.yml:/usr/share/logstash/config/logstash.yml \
+ -v $(pwd)/config/logstash-gc.conf:/usr/share/logstash/pipeline/logstash.conf \
+ -v $(pwd)/config/interim:/usr/share/logstash/vendor/bundle/jruby/2.3.0/gems/logstash-patterns-core-4.1.2/patterns/interim \
  docker.elastic.co/logstash/logstash:6.2.4
