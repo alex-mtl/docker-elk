4f5616b5f7086703cf6a0dc74594f71071ae6a35
a.kim <alexander.kim.ca@gmail.com>
2018-05-03 15:45:08 -0400

    update

--- filebeat-gc.sh
+++ filebeat-gc.sh
@@ -4,4 +4,5 @@ docker run -d --name filebeat-gc \
  --link logstash-gc:logstash \
   -v $(pwd)/config/filebeat-gc.yml:/usr/share/filebeat/filebeat.yml \
   -v $(pwd)/config/interim.prospector-gc.yml:/usr/share/filebeat/prospectors.d/interim.prospector.yml \
+  -v /var/www/html/webservice_interim/mdcl:/mnt/log/import \
    docker.elastic.co/beats/filebeat:6.2.4
