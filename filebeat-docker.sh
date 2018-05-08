4074efe7a3e74b53efc177519bbd39fa85c1da66
kim.alexander <kim.alexander.ca@gmail.com>
2018-05-08 10:41:14 -0400

    update

--- filebeat-docker.sh
+++ filebeat-docker.sh
@@ -1,6 +1,7 @@
-docker kill filebeat-gc-new
-docker rm filebeat-gc-new
-docker run -d --name filebeat-gc-new \
+docker kill filebeat-gc
+docker rm filebeat-gc
+docker run -d --name filebeat-gc \
+ --link logstash-gc:logstash \
   -v $(pwd)/config/filebeat-gc.yml:/usr/share/filebeat/filebeat.yml \
   -v $(pwd)/config/interim.prospector-gc.yml:/usr/share/filebeat/prospectors.d/interim.prospector.yml \
   -v /var/www/html/webservice_interim/mdcl:/mnt/log/import \
